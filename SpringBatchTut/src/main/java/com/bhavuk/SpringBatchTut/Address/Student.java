2f041e24ec1ed579a87b4b07d4085acb2191d274
Gupta <bgupta@transformco.com>
2022-12-27 23:07:30 +0530

    update002

---
+++ SpringBatchTut/src/main/java/com/bhavuk/SpringBatchTut/Address/Student.java
@@ -0,0 +1,70 @@
+package com.bhavuk.SpringBatchTut.Address;
+
+public class Student {
+	
+	private String Last_name;
+	private String First_name;
+	private double Phone;
+	private String Email;
+	private String Title;
+	private String Designation;
+	
+	public Student() {}
+
+	public Student(String last_name, String first_name, double phone, String email, String title, String designation) {
+		super();
+		Last_name = last_name;
+		First_name = first_name;
+		Phone = phone;
+		Email = email;
+		Title = title;
+		Designation = designation;
+	}
+	public String getLast_name() {
+		return Last_name;
+	}
+	public void setLast_name(String last_name) {
+		Last_name = last_name;
+	}
+	public String getFirst_name() {
+		return First_name;
+	}
+	public void setFirst_name(String first_name) {
+		First_name = first_name;
+	}
+	public double getPhone() {
+		return Phone;
+	}
+	public void setPhone(double phone) {
+		Phone = phone;
+	}
+	public String getEmail() {
+		return Email;
+	}
+	public void setEmail(String email) {
+		Email = email;
+	}
+	public String getTitle() {
+		return Title;
+	}
+	public void setTitle(String title) {
+		Title = title;
+	}
+	public String getDesignation() {
+		return Designation;
+	}
+	public void setDesignation(String designation) {
+		Designation = designation;
+	}
+	@Override
+	public String toString() {
+		return "InterfaceImplementation [Last_name=" + Last_name + ", First_name=" + First_name + ", Phone=" + Phone
+				+ ", Email=" + Email + ", Title=" + Title + ", Designation=" + Designation + "]";
+	}
+	
+	
+	
+
+
+	
+}
